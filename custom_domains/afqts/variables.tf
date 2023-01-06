da8e83822434644825971ae5829038fae30b1880
NickGraham101 <nick.graham101@googlemail.com>
2023-01-06 16:10:52 +0000

    Removed unused tags from locals

--- custom_domains/afqts/variables.tf
+++ custom_domains/afqts/variables.tf
@@ -6,16 +6,4 @@ variable "environment_tag" {}
 variable "ruleset_name" {}
 
 locals {
-  default_tags = {
-    "Parent Business": "Teacher Training and Qualifications",
-    "Portfolio": "Early Years and Schools Group",
-    "Product": "Database of Qualified Teachers",
-    "Service": "Teacher Training and Qualifications",
-    "Service Line": "Teaching Workforce",
-    "Service Offering": "Database of Qualified Teachers"
-  }
-  tags = merge(
-    local.default_tags,
-    { "Environment" = var.environment_tag }
-  )
 }
