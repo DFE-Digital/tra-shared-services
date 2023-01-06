aa5d684b1caaa992e87829657ea5310d19f2c7ee
NickGraham101 <nick.graham101@googlemail.com>
2023-01-06 14:14:01 +0000

    Update afqts terraform to use v0.4.0 of module

--- custom_domains/afqts/main.tf
+++ custom_domains/afqts/main.tf
@@ -1,9 +1,12 @@
 module "domains" {
-  source              = "git::https://github.com/DFE-Digital/terraform-modules.git//domains/environment_domains"
+  source              = "git::https://github.com/DFE-Digital/terraform-modules.git//domains/environment_domains?ref=v0.4.0"
   zone                = var.zone
   front_door_name     = var.front_door_name
   resource_group_name = var.resource_group_name
   domains             = var.domains
+  environment         = var.environment_short
+  host_name           = var.origin_hostname
+  rule_set_ids        = [azurerm_cdn_frontdoor_rule_set.ruleset.id]
 }
 
 data "azurerm_cdn_frontdoor_profile" "main" {
@@ -11,10 +14,39 @@ data "azurerm_cdn_frontdoor_profile" "main" {
   resource_group_name = var.resource_group_name
 }
 
+data "azurerm_dns_zone" "main" {
+  name                = var.zone
+  resource_group_name = var.resource_group_name
+}
+
 resource "azurerm_cdn_frontdoor_rule_set" "ruleset" {
-  name                     = var.ruleset_name
+  name                     = var.environment_short
   cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.main.id
 }
 
-# TODO: Create rule
-# TODO: Associate rule to route
+resource "azurerm_cdn_frontdoor_rule" "rule" {
+  for_each = var.redirect_rules
+  depends_on = [module.domains]
+  name                      = replace(
+                                replace(each.key, ".", ""),
+                                "-", "")
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.ruleset.id
+  order                     = 1
+  behavior_on_match         = "Continue"
+
+  actions {
+
+    url_redirect_action {
+      redirect_type        = "Moved"
+      redirect_protocol    = "Https"
+      destination_hostname = each.value
+    }
+  }
+
+  conditions {
+    host_name_condition {
+      operator         = "Equal"
+      match_values     = [each.key]
+    }
+  }
+}
